12d9e1de553f5a5ca6ace07a0a944a7a99c56d2b
therealmcfly <eugene.fly.lee@gmail.com>
2024-08-14 16:04:14 +1200

    create basic chatbot

---
+++ basic-chatbot.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o")
+
+chat_history = []
+
+system_message = "You are a AI chatbot."
+chat_history.append(system_message)
+
+# Chat loop
+while True:
+    query = input("You: ")
+    if query == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result = model.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response))
+
+    print("AI: ", response)
